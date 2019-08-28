897ca83233b43a42c4fe971b047765276159b7d9
xuantung <tungdx@smartosc.com>
2019-08-28 16:34:19 +0700

    Update version 19.08.27

--- Block/Info/RetailMultiple.php
+++ Block/Info/RetailMultiple.php
@@ -63,6 +63,7 @@ class RetailMultiple extends Info
      * Enter description here...
      *
      * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
     public function getMultiplePaymentData()
     {
